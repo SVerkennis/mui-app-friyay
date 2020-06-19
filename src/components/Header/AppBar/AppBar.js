1961e67b300c45dd6c4e2d87c9724474bbd2f3fc
Sabine Verkennis <s.verkennis@yahoo.de>
2020-06-19 12:38:41 +0200

    Add style for AppBar and TitleImage

--- src/components/Header/AppBar/AppBar.js
+++ src/components/Header/AppBar/AppBar.js
@@ -1,11 +1,36 @@
-import React from "react";
 import "./AppBar.css"
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 
-export default function AppBar() {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+export default function ButtonAppBar() {
+    const classes = useStyles();
+
     return (
-        <>
-
-        </>
-
+        <div className={classes.root}>
+            <AppBar position="sticky">
+                <Toolbar className={"tools"}>
+                    <Button color="inherit">Home</Button>
+                    <Button color="inherit">Asia</Button>
+                    <Button color="inherit">America</Button>
+                    <Button color="inherit">Africa</Button>
+                    <Button color="inherit">Australia</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
     );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,21 @@ import AppBar from "./AppBar/AppBar";
 import "./Header.css"
 
 export default function Header() {
+
+    const mainFeaturedPost = {
+        title: 'TRAVEL',
+        description:
+            "is the only thing you buy that makes you richer",
+        image: 'https://cdn.pixabay.com/photo/2019/01/27/22/32/mountains-3959204_960_720.jpg',
+        imgText: 'main image description',
+        // linkText: 'Continue reading…',
+    };
+
+
     return (
 
         <header>
-
-        <TitleImage></TitleImage>
+        <TitleImage post={mainFeaturedPost}></TitleImage>
         <AppBar></AppBar>
 
         </header>
